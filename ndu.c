81eb0517d26c2c0123c3a9a01f5fd3c1934a29fd
Kevin Pors <krpors@gmail.com>
2018-07-09 23:09:20 +0200

    directory traversal via custom method
    needs further refinement but its a start

--- ndu.c
+++ ndu.c
@@ -3,11 +3,48 @@
 #include <string.h>
 #include <ftw.h>
 
+#include <unistd.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
 #include "fs.h"
 
 static int callback(const char* fpath, const struct stat* sb, int typeflag, struct FTW* ftwbuf) {
 	printf("fpath: %s\n", fpath);
 	return 0;
+}
+
+void printdir(const char *dir, int depth) {
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+	int spaces = depth*4;
+
+	if((dp = opendir(dir)) == NULL) {
+		fprintf(stderr,"cannot open directory: %s\n", dir);
+		return;
+	}
+	chdir(dir);
+	while((entry = readdir(dp)) != NULL) {
+		lstat(entry->d_name,&statbuf);
+		if(S_ISDIR(statbuf.st_mode)) {
+		/* Found a directory, but ignore . and .. */
+			if(strcmp(".",entry->d_name) == 0 || strcmp("..",entry->d_name) == 0) {
+				continue;
+			}
+
+			printf("%*s%s/\n",spaces,"",entry->d_name);
+			/* Recurse at a new indent level */
+			printdir(entry->d_name,depth+1);
+		}
+		else {
+			printf("%*s%s\n",spaces,"",entry->d_name);
+		}
+	}
+	chdir("..");
+	closedir(dp);
 }
 
 int main(int argc, char* argv[]) {
@@ -27,5 +64,5 @@ int main(int argc, char* argv[]) {
 
 	dir_free(root);
 
-	int x = nftw("/home", callback, 20, 0);
+	printdir("/home/krpors/Development", 0);
 }
